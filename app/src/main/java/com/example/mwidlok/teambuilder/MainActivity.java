40aac802266ff1b443a8aaf706ac1296d9e0d985
MWidlok <m.widlok@immowelt.de>
2017-06-26 17:08:35 +0200

    fab snackbar included

--- app/src/main/java/com/example/mwidlok/teambuilder/MainActivity.java
+++ app/src/main/java/com/example/mwidlok/teambuilder/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.mwidlok.teambuilder;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fabNewEvent);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Do someting",Snackbar.LENGTH_LONG).setAction("Action",null).show();
+            }
+        });
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rvEventsView);
         mRecyclerView.setHasFixedSize(true);
