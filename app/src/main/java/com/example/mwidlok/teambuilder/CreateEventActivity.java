b4b49a656caa023cea3ff52a851fa9a6b6c6a81c
MWidlok <m.widlok@immowelt.de>
2017-07-06 08:00:33 +0200

    proceed create event activity

--- app/src/main/java/com/example/mwidlok/teambuilder/CreateEventActivity.java
+++ app/src/main/java/com/example/mwidlok/teambuilder/CreateEventActivity.java
@@ -2,12 +2,35 @@ package com.example.mwidlok.teambuilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    Button btnSaveEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        btnSaveEvent = (Button) findViewById(R.id.btnSave);
+
+        btnSaveEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = new Toast(getApplicationContext());
+                t.setText("Button create event clicked.");
+                t.show();
+
+            }
+        });
     }
+
+
+
+
+
+
 }
